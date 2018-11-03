c5d95a04497dd806cba6ca4766920e91e6b029f7
Your Name <magejohnyjtp@gmail.com>
2018-11-03 19:33:53 +0000

    small changes to routing

--- teamup/index.py
+++ teamup/index.py
@@ -1,9 +1,12 @@
-from flask import Flask , render_template
+from flask import Flask, render_template, redirect, url_for
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
-    return render_template("home/index.html" , name="dsad")
+    return render_template("home/index.html", name="dsad")
+
 
 @app.route("/find")
 def find():
@@ -14,14 +17,21 @@ def find():
 def createEvent():
     return "Create a Tea event"
 
-@app.route("/event/edit/<id>")
-def editEvent(id):
-    return "Edit event " + str(id)
+
+@app.route("/event/edit/")
+@app.route("/event/edit/<ident>")
+def editEvent(ident=None):
+    if ident is not None:
+        return "Edit event " + str(ident)
+    return redirect(url_for('index'))
 
 
+@app.route("/event/view/")
 @app.route("/event/view/<id>")
-def viewEvent(id):
-    return "View event "+ str(id)
+def viewEvent(ident=None):
+    if ident is not None:
+        return "View event " + str(ident)
+    return redirect(url_for('index'))
 
 
 @app.route("/login")
@@ -32,6 +42,7 @@ def login():
 @app.errorhandler(404)
 def pageNotFound(error):
     return render_template('page_not_found.html'), 404
+
 
 if __name__ == "__main__":
     app.run(debug=True)
