c7441e5417a768cc73f17fd77df44e5c02541e4e
Unknown <SleepDeprivedNerds@gmail.com>
2018-11-03 14:00:03 +0000

    Fixed event passing variable

--- teamup/helloworld.py
+++ teamup/helloworld.py
@@ -2,8 +2,34 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return "Home Page"
+
+@app.route("/find")
+def find():
+    return "Find a tea place"
+
+
+@app.route("/create")
+def createEvent():
+    return "Create a Tea event"
+
+@app.route("/event/edit/<id>")
+def editEvent(id):
+    return "Edit event " + str(id)
+
+
+@app.route("/event/view/<id>")
+def viewEvent(id):
+    return "View event "+ str(id)
+
+
+@app.route("/login")
+def login():
+    return "Login"
+
+
+
 
 if __name__ == "__main__":
     app.run()
